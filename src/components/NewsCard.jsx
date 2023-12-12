15877a9eefd18f0084701bef389177a8b2ba4bae
Nirajan Bikram Malla Thakuri <125073944+NirzanMalla@users.noreply.github.com>
2023-12-12 20:48:55 +0545

    Worked on Readmore Button (#4)

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -5,6 +5,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
+import { NavLink } from "react-router-dom";
 
 const NewsCard = ({ value }) => {
   return (
@@ -24,22 +25,40 @@ const NewsCard = ({ value }) => {
           value?.urlToImage ||
           "https://static.vecteezy.com/system/resources/thumbnails/004/216/831/original/3d-world-news-background-loop-free-video.jpg"
         }
-        title={value?.title || "default image"}
+        style={{
+          objectFit: "contain",
+        }}
       />
 
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {value?.description ?? "This is the default description section"}
+        <Typography color="black" style={{ fontSize: "12px" }}>
+          By-{value?.author ?? "Nirajan"}
+          <br />
+          Published at: {value?.publishedAt ?? "2023-11-23"}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {value?.content ?? "This is the default content section"}
+        <Typography gutterBottom variant="h5" component="div">
+          {value?.title ?? "This is the default title section"}
+        </Typography>
+        <Typography color="text.secondary" style={{ fontSize: "14px" }}>
+          {value?.description ?? "This is the default description section"}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" variant="contained" color="success">
-          Share
+        <Button
+          size="small"
+          variant="contained"
+          color="success"
+          style={{ marginRight: "5px" }}
+        >
+          Save
         </Button>
-        <Button size="small" variant="outlined" color="success">
+        <Button
+          size="small"
+          variant="outlined"
+          color="success"
+          component={NavLink}
+          to={value?.url}
+        >
           Read More
         </Button>
       </CardActions>
