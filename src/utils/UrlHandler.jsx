81afa871cb7dfecfaf01e57d5287f7c91e49c96a
Nirajan <nirzanmalla@gmail.com>
2023-12-15 20:41:44 +0545

    Feature-Select-Countries implemented

--- src/utils/UrlHandler.jsx
+++ src/utils/UrlHandler.jsx
@@ -1,6 +1,11 @@
-const UrlHandler = (query) => {
+const UrlHandler = (query, country) => {
   const apikey = import.meta.env.VITE_API_KEY;
-  const url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apikey}`;
+  // let url = `https://newsapi.org/v2/everything?q=${query}&apiKey=${apikey}`;
+  let url = `https://newsapi.org/v2/top-headlines?q=${query}&country=${country}&apiKey=${apikey}`;
+
+  // if (country) {
+  //   url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${apikey}`;
+  // }
   return url;
 };
 export default UrlHandler;
